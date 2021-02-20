da13ded858f15d58d73185fb36e7b6223647a2b8
robbie <robbielcampbell@hotmail.com>
2021-02-20 14:06:01 +0000

    :rocket: learning vue :rocket:

---
+++ Vue/vue_crash_todolist/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste id, adipisci corporis fugiat atque unde expedita accusamus nobis dignissimos vero autem aliquid dolor mollitia quod eius culpa accusantium blanditiis minus?</p>
+  </div>
+</template>
